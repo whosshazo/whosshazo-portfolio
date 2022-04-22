6166626430aec0aa3c72cb5a14e9ce6a56d3804b
whosshazo <ryandeshazo1@gmail.com>
2022-04-22 16:37:56 -0400

    current projects page

--- whosshazo-portfolio/src/components/Projects/projects.js
+++ whosshazo-portfolio/src/components/Projects/projects.js
@@ -7,29 +7,67 @@ import {
   CardLink,
   CardText,
 } from "reactstrap";
+import Sk8spotter from "../../assets/selfie/sk8spotter.png";
+import Notes from "../../assets/selfie/note-taker.png";
+import Cocktails from "../../assets/selfie/cocky-cocktails.png";
 
 function Projects(props) {
   return (
-    <div>
-      <Card>
+    <div className="d-flex p-2 ">
+      <Card className="p-2">
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6">
-            Card subtitle
-          </CardSubtitle>
+          <CardTitle tag="h5"><u>Sk8.Spotter</u></CardTitle>
         </CardBody>
-        <img
-          alt=""
-          src=""
-          width="100%"
-        />
+        <img alt="ska8spotter home page" src={Sk8spotter} width="100%" />
         <CardBody>
           <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            This application makes finding skateparks easier in the local
+            Richmond, VA area. User can view photos, comments and even rate the
+            parks for other users.
           </CardText>
-          <CardLink href="#">Card Link</CardLink>
-          <CardLink href="#">Another Link</CardLink>
+          <CardLink href="https://github.com/sora64/sk8spotter">
+            GitHub
+          </CardLink>
+          <CardLink href="https://sk8spotter.herokuapp.com/">URL</CardLink>
+        </CardBody>
+      </Card>
+      <Card className="p-2">
+        <CardBody>
+          <CardTitle tag="h5"><u>Note Taker</u></CardTitle>
+        </CardBody>
+        <img alt="ska8spotter home page" src={Notes} width="100%" />
+        <CardBody>
+          <CardText>
+            With the help of Express.js, Note Taker Plus creates and saves a
+            users's notes then deleted when the user is finished with them.
+          </CardText>
+          <CardLink href="https://github.com/whosshazo/note-taker-express">
+            GitHub
+          </CardLink>
+          <CardLink href="https://lit-sands-64855.herokuapp.com/">
+            Live Site
+          </CardLink>
+        </CardBody>
+      </Card>
+      <Card className="p-2">
+        <CardBody>
+          <CardTitle tag="h5"><u>Cocky Cocktails</u></CardTitle>
+        </CardBody>
+        <img alt="ska8spotter home page" src={Cocktails} width="100%" />
+        <CardBody>
+          <CardText>
+            Cocky-Cocktails is an application for aspiring bartenders. This
+            applications will give cocktatil instructions from the availible
+            supplies in your cupboard. Once a cocktail is selected, the user
+            will be giving mixing instructions and a YouTube video on how to
+            make the drink.
+          </CardText>
+          <CardLink href="https://github.com/whosshazo/cocktail-project">
+            GitHub
+          </CardLink>
+          <CardLink href="https://whosshazo.github.io/cocktail-project/">
+            URL
+          </CardLink>
         </CardBody>
       </Card>
     </div>
